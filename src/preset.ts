efce32985127981db2a400b29dd13a7ff5c7ce3e
Benoit Briatte <briatte.benoit@digipolitan.com>
2019-04-29 00:14:39 +0200

    fix preset id wrong type

--- src/preset.ts
+++ src/preset.ts
@@ -2,7 +2,7 @@ import {XMLElement} from './utils/xml-element';
 import {ContentItem, contentItemFromElement} from './content-item';
 
 export interface Preset {
-    readonly id: string,
+    readonly id: number,
     readonly createdDate: Date,
     readonly updatedDate: Date,
     readonly contentItem: ContentItem
@@ -14,7 +14,7 @@ export function presetFromElement(element: XMLElement): Preset | undefined {
         return undefined;
     }
     return {
-        id: element.getAttribute('id'),
+        id: parseInt(element.getAttribute('id')),
         createdDate: new Date(parseInt(element.getAttribute('createdOn')) * 1000),
         updatedDate: new Date(parseInt(element.getAttribute('updatedOn')) * 1000),
         contentItem: contentItemFromElement(element.getChild('ContentItem'))
